a35269ec63566103bbb2101e0c97793eb95e98c4
james <alissonbruno.sa@gmail.com>
2013-09-21 11:40:18 -0300

    Modificacao

--- Gemfile
+++ Gemfile
@@ -10,7 +10,10 @@ gem 'friendly_id',  "~> 4.0.10.1"
 gem 'kaminari'
 gem 'carrierwave'
 gem 'rmagick'
-gem 'pg'
+group :production do
+  gem 'pg'
+end
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,18 @@ ColchonetBootstrap::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+
+  config.action_mailer.default_url_options = {
+      :host => "floating-cove-3454.herokuapp.com"
+  }
+  config.action_mailer.smtp_settings = {
+      :port => ENV['MAILGUN_SMTP_PORT'],
+      :address => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+      :password => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain => 'calm-badlands-8648.herokuapp.com',
+      :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
 end
